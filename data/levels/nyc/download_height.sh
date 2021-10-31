4c3a4fcc90836933f0c6ba11e4fd82946549f73b
Your Name <you@example.com>
2021-10-31 11:38:01 +0100

    Update download heightmap

--- data/levels/nyc/download_height.sh
+++ data/levels/nyc/download_height.sh
@@ -5,12 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 #   https://terrain.party/
 #   https://tangrams.github.io/heightmapper
 
-# curl -o terrain.zip http://terrain.party/api/export?box=10.4701,51.4688,10.491,51.4811
-
-# curl -o terrain-wr.zip http://terrain.party/api/export?box=10.7074300,49.7492800,10.7234400,49.7556500
-
-# curl -o terrain-er.zip http://terrain.party/api/export?box=10.9976000,49.5892900,11.0136100,49.5956800
-
 curl -o terrain-nyc.zip http://terrain.party/api/export?box=-74.0231000,40.7030000,-73.9911000,40.7180000
 
 ../../mlib/UDebug/Apps/Download_Heightmap/download_heightmap \
