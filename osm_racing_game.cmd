aad80b0b8173af17f187f77b80fa4fcef18a94d8
Your Name <you@example.com>
2021-11-20 18:41:20 +0100

    Fullscreen resolution

--- osm_racing_game.cmd
+++ osm_racing_game.cmd
@@ -3,6 +3,6 @@ GMRelease\render_scene_file.exe ^
     data\levels\main\main.scn ^
     --nsamples_msaa 2 ^
     --show_mouse_cursor ^
-    --screen_width 1500 ^
-    --screen_height 900
+    --windowed_width 1500 ^
+    --windowed_height 900
 pause
