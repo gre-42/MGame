f3943319d9bc72a870ba4360040a822be6b8f253
Marc Kramer <marc.kramer@polyfit42.com>
2022-09-07 23:10:30 +0200

    Update thumbs

--- data/models/opengameart/Facade/create_thumbnails.sh
+++ data/models/opengameart/Facade/create_thumbnails.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 for f in *.obj; do
-    ../../../../mlib/URelease/Apps/Render_Obj_File/render_obj_file $f --output /tmp/out.ppm
-    convert /tmp/out.ppm -trim -transparent 'rgb(255,0,255)' ${f%%.obj}_thumb.png
-    rm /tmp/out.ppm
+    render_obj_file "$f" --output /tmp/out.png
+    convert /tmp/out.png -trim -transparent 'rgb(255,0,255)' "${f%%.obj}_thumb.png"
+    rm /tmp/out.png
 done;
