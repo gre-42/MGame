b1d4b124ac52fb40a95521fdb63a6ef8f58a1577
Marc Kramer <marc.kramer@polyfit42.com>
2022-10-15 21:51:00 +0200

    Start retro raceway

--- data/models/opengameart/Hedge/create_files_one.sh
+++ data/models/opengameart/Hedge/create_files_one.sh
@@ -4,21 +4,17 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 seed=1
 for dir in Small Long SmallRound LongRound; do
-    make -C /home/kl/MySrc/mlib release
     cd $dir
     for angle_y in 45 135; do
-        /home/kl/MySrc/mlib/URelease/Apps/Render_Obj_File/render_obj_file \
+        render_obj_file \
             *Exp.obj \
-            --nsamples_msaa 0 \
-            --blend_mode binary \
-            --aggregate_mode off \
-            --scale 0.2 \
-            --output out.ppm \
-            --width 2048 \
-            --height 2048 \
-            --y -5 \
+            --nsamples_msaa 1 \
+            --blend_mode binary_05 \
+            --scale 0.7 \
+            --output /tmp/out.png \
+            --look_at_aabb \
+            --y -10 \
             --light_configuration one \
-            --no_cull_faces \
             --no_shadows \
             --angle_y $angle_y \
             --light_x 0 \
@@ -26,9 +22,11 @@ for dir in Small Long SmallRound LongRound; do
             --light_z -40 \
             --light_angle_x -90 \
             --light_angle_y 90 \
-            --light_angle_z 45
-        convert out.ppm -trim -transparent 'rgb(255,0,255)' card_$angle_y.png
-        rm out.ppm
+            --light_angle_z 45 \
+            --diffusivity 1 \
+            --specularity 0
+        convert /tmp/out.png -trim -transparent 'rgb(255,0,255)' card_$angle_y.png
+        rm /tmp/out.png
     done
     cd -
 done
