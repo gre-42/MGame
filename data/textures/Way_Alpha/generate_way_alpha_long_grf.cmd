4c33ea699c838d2e50d1d427bed5381346653bb8
Marc Kramer <marc.kramer@mymail.com>
2023-11-25 14:14:53 +0100

    GRF mud

---
+++ data/textures/Way_Alpha/generate_way_alpha_long_grf.cmd
@@ -0,0 +1,10 @@
+python ..\..\..\Mlib\Mlib\Render\blend_terrain_street.py ^
+	--detail ^
+		"<bilinear>" ^
+		"<bigrf>" ^
+	--width 128 ^
+	--height 1024 ^
+	--detail_fac 2 2 ^
+	--detail_offset -0.3 0 ^
+	--result Way_Alpha_Long_Grf.png
+pause
