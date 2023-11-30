d6eb5dab4264d12401a6650dcb4adb10d98b2094
Marc Kramer <marc.kramer@mymail.com>
2023-11-30 17:50:17 +0100

    Add bushmap

---
+++ data/textures/Bush_Alpha/generate_bush_alpha.cmd
@@ -0,0 +1,11 @@
+python ..\..\..\Mlib\Mlib\Render\blend_terrain_street.py ^
+	--detail ^
+		..\hf_heightmap.png[-2] ^
+		..\hf_heightmap.png[0] ^
+	--width 1024 ^
+	--height 1024 ^
+	--detail_fac 4 50 ^
+	--detail_offset -0.5 -0.5 ^
+	--foreground ..\textures.com\bushes\TexturesCom_Trees0029_masked_S_Tile_Seamless.png[2] ^
+	--background ..\cc0textures\Ground037_2K_Color.jpg[2]
+pause
