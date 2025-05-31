7804fe6fa992573cc615ced1fba7b72e76c191e6
Marc Kramer <gre@die-optimisten.net>
2025-05-31 22:29:08 +0200

    MIPMAP_2D start

--- data/textures/prococean/generate_proc_ocean.sh
+++ data/textures/prococean/generate_proc_ocean.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eux
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-../../../Mlib/GURelWithDebInfo/Bin/proc_terrain_perlin --frequency 32 --octaves 8 --seed 2 --size 1024 --out /tmp/heightmap.pgm --sigma 6 --alpha 0.5 --min 0.1 --max 1 --invert --seamless_overlap 100
-convert /tmp/heightmap.pgm /tmp/heightmap.png
+for seed in 2 3; do
+    ../../../Mlib/GURelWithDebInfo/Bin/proc_terrain_perlin --frequency 32 --octaves 8 --seed $seed --size 512 --out heightmap_$seed.png --sigma 6 --alpha 0.5 --min 0.1 --max 1 --invert --seamless_overlap 50
+done
+# convert /tmp/heightmap.pgm /tmp/heightmap.png
 # NormaMap.png created using https://cpetry.github.io/NormalMap-Online
